66ce2ac36cc3a7d8ac41b97abcc461eb5acb6a55
René Bühling <rene.buehling@gmx.de>
2019-04-08 15:28:46 +0200

    + Teil 1

---
+++ Teil 01 - Basis mit Umdrehen/Scripts/Karte.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+public class Karte : MonoBehaviour
+{
+    /// <summary>
+    /// Zeiger auf das Bild der umgedrehten Karte.
+    /// </summary>
+    public Sprite kartenbild;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("OnClick wurde aufgerufen.");
+
+        GetComponent<PlayableDirector>().Play();        
+    }
+
+    public void TauscheKartenbild()
+    {
+        GetComponent<Image>().sprite = kartenbild;
+    }
+
+}
