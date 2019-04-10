cad726eeecbba6534ec03ba6bf1442ef5398cb9a
René Bühling <rene.buehling@gmx.de>
2019-04-10 14:43:28 +0200

    + Init

---
+++ Teil 02 - Kartenstapel mit Prefabs/Scripts/Karte.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+/// <summary>
+/// Zeichnet die Daten einer Kartenbeschreibung in der Szene
+/// und regelt die Interaktionsmöglichkeiten.
+/// </summary>
+public class Karte : MonoBehaviour
+{
+    /// <summary>
+    /// Zeiger auf das Bild der umgedrehten Karte.
+    /// </summary>
+    public Sprite kartenbild;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private bool KarteSchonUmgedreht()
+    {
+        return GetComponent<Image>().sprite == kartenbild;
+    }
+
+    private bool UmdrehenGestartet()
+    {
+        return GetComponent<PlayableDirector>().state == PlayState.Playing;
+    }
+
+    public void OnClick()
+    {
+        if (UmdrehenGestartet() || KarteSchonUmgedreht()) { return;  }
+
+        Debug.Log("OnClick wurde aufgerufen. ");
+
+        GetComponent<PlayableDirector>().Play();        
+    }
+
+    public void TauscheKartenbild()
+    {
+        if (!Application.isPlaying) { return; }
+
+        Debug.Log("Jetzt wird das Kartenbild getauscht.");
+        GetComponent<Image>().sprite = kartenbild;
+    }
+
+}
---
+++ Teil 02 - Kartenstapel mit Prefabs/Scripts/Kartenstapel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Zeichnet den Kartenstapel und regelt seine Interaktion.
+/// </summary>
+public class Kartenstapel : MonoBehaviour
+{
+    public List<Kartenbeschreibung> karten;
+
+    //public Image kartendarstellung;
+
+    public GameObject kartenPrefab;
+
+    public GameObject kartenContainer;
+
+    public void KlickAufStapel()
+    {
+        Debug.Log("Klick auf den Stapel.");
+        if (karten.Count == 0)
+        {
+            Debug.Log("Der Stapel ist leer!");
+            return;
+        }
+
+        int zufaelligeStapelPosition = Random.Range(0,karten.Count);
+
+        Kartenbeschreibung gezogeneKarte = karten[zufaelligeStapelPosition];
+        Debug.Log("Gezogene Karte = " + gezogeneKarte.kartenbild + " von Position="+zufaelligeStapelPosition);
+        karten.Remove(gezogeneKarte);
+
+        //kartendarstellung.sprite = gezogeneKarte.kartenbild;
+        //kartendarstellung.gameObject.SetActive(true);
+        GameObject karteInSzene = Instantiate(kartenPrefab,kartenContainer.transform);
+        karteInSzene.GetComponent<Image>().sprite = gezogeneKarte.kartenbild;
+
+        if (karten.Count == 0) gameObject.SetActive(false);
+    }
+}
