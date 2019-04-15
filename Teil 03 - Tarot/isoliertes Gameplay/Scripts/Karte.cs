9f2141eb9da67becb70a9c0d1473383706e6bbbe
René Bühling <rene.buehling@gmx.de>
2019-04-15 18:14:41 +0200

    Init

---
+++ Teil 03 - Tarot/isoliertes Gameplay/Scripts/Karte.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+/// <summary>
+/// Zeichnet die Daten einer Kartenbeschreibung in der Szene
+/// und regelt die Interaktionsmöglichkeiten.
+/// </summary>
+public class Karte : MonoBehaviour
+{
+    /// <summary>
+    /// Zeiger auf die Daten der umgedrehten Karte.
+    /// </summary>
+    private Kartenbeschreibung beschreibung;
+
+    /// <summary>
+    /// Weist der Kartendarstellung einen neuen Kartenwert zu.
+    /// Die Karte ist sofort sichtbar, wenn kannAngeklicktWerden true ist.
+    /// </summary>
+    /// <param name="neueBeschreibung">Neuer Wert der Karte.</param>
+    /// <param name="kannAngeklicktWerden">Wenn true, spielt die Karte bei Klick die Umdrehanimation ab.</param>
+    public void setzeBeschreibung(Kartenbeschreibung neueBeschreibung, bool kannAngeklicktWerden)
+    {
+        beschreibung = neueBeschreibung;
+        if (!kannAngeklicktWerden) { GetComponent<Image>().sprite = neueBeschreibung.kartenbild; }
+        GetComponent<Button>().enabled = kannAngeklicktWerden;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private bool KarteSchonUmgedreht()
+    {
+        return GetComponent<Image>().sprite == beschreibung.kartenbild;
+    }
+
+    private bool UmdrehenGestartet()
+    {
+        return GetComponent<PlayableDirector>().state == PlayState.Playing;
+    }
+
+    public void OnClick()
+    {
+        if (UmdrehenGestartet() || KarteSchonUmgedreht()) { return;  }
+
+        Debug.Log("OnClick wurde aufgerufen. ");
+
+        GetComponent<PlayableDirector>().Play();        
+    }
+
+    public void TauscheKartenbild()
+    {
+        if (!Application.isPlaying) { return; }
+
+        Debug.Log("Jetzt wird das Kartenbild getauscht.");
+        GetComponent<Image>().sprite = beschreibung.kartenbild;
+    }
+
+}
---
+++ Teil 03 - Tarot/isoliertes Gameplay/Scripts/SpielprinzipTarot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpielprinzipTarot : MonoBehaviour
+{
+    public int verbleibendeZuege = 4;
+
+    public Text hilfetext;
+
+    private void aktualisiereHilfetext()
+    {
+        if (verbleibendeZuege == 0)
+        {
+            hilfetext.text = "Sieh Dir die Karten an, die Du gezogen hast.";
+        }
+        else
+        {
+            hilfetext.text = "Ziehe " + verbleibendeZuege + " Karten!";
+        }
+    }
+
+
+    private void Start()
+    {
+        aktualisiereHilfetext();
+    }
+
+    public void KlickAufStapel()
+    {
+        Kartenstapel stapel = GetComponent<Kartenstapel>();
+
+        Debug.Log("Klick auf den Stapel.");
+        if (stapel.istLeer())
+        {
+            Debug.Log("Der Stapel ist leer!");
+            return;
+        }
+
+        Kartenbeschreibung gezogeneKarte = stapel.hebeZufaelligeKarteAb();
+        verbleibendeZuege -= 1; // ist das gleiche wie verbleibendeZuege = verbleibendeZuege - 1;
+
+        stapel.erzeugeKarteInSzene().setzeBeschreibung(gezogeneKarte, false);
+
+        aktualisiereHilfetext();
+
+        if (stapel.karten.Count == 0 || verbleibendeZuege == 0) gameObject.SetActive(false);
+    }
+
+
+}
