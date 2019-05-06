c4d1a98162a0270bb0fee4c8ca5ceb6e3e454959
René Bühling <rene.buehling@gmx.de>
2019-05-06 15:37:15 +0200

    Übung Paare

---
+++ Teil 05 - Paare/mit Uebung/Scripts/Kartenbeschreibung.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enthält die inhaltlichen Daten einer Karte.
+/// Die Darstellung, Interaktion, usw. in der Programmoberfläche
+/// geschieht über Karte.cs.
+/// </summary>
+[System.Serializable]
+public class Kartenbeschreibung
+{
+    /// <summary>
+    /// Zeiger auf das Bild der umgedrehten Karte.
+    /// </summary>
+    public Sprite kartenbild;
+}
