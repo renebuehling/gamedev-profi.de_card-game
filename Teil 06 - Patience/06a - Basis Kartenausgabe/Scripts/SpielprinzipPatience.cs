7b6551dc41a986fb630bd1e0f3084be4694187e6
René Bühling <rene.buehling@gmx.de>
2019-05-13 10:58:35 +0200

    Init

---
+++ Teil 06 - Patience/06a - Basis Kartenausgabe/Scripts/SpielprinzipPatience.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpielprinzipPatience : MonoBehaviour
+{
+    /// <summary>
+    /// Zeichner für die abgehobene Karte, bevor sie auf dem Tisch platziert wird.
+    /// </summary>
+    public Karte vorschauKarte;
+
+    private void Start()
+    {
+        Kartenstapel stapel = GetComponent<Kartenstapel>(); //Verweis auf Kartenstapelscript
+        stapel.mischeStapel();
+        KlickAufStapel();
+    }
+
+    public void KlickAufStapel()
+    {
+        Debug.Log("Klick auf Stapel");
+        Kartenstapel stapel = GetComponent<Kartenstapel>(); //Verweis auf Kartenstapelscript
+
+        if (vorschauKarte.aktuelleBeschreibung()!=null)
+        {
+            stapel.legeKarteZurueck(vorschauKarte.aktuelleBeschreibung());
+        }
+
+        if (stapel.istLeer())
+        {
+            stapel.gameObject.SetActive(false);
+        }
+        else
+        {
+            Kartenbeschreibung gezogeneKarte = stapel.hebeObersteKarteAb(); //Karte vom Stapel abheben (nur Wert)
+            vorschauKarte.setzeBeschreibung(gezogeneKarte, false);
+
+            if (stapel.istLeer())
+            {
+                stapel.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void LegeKarteAuf(StapelContainer container)
+    {
+        if(vorschauKarte.aktuelleBeschreibung()==null) //keine Karte aufgedeckt oder übrig
+        {
+            return;
+        }
+
+        Debug.Log("Lege die Karte auf "+container);
+        Kartenstapel stapel = GetComponent<Kartenstapel>(); //Verweis auf Kartenstapelscript
+        stapel.kartenContainer = container.gameObject; //dem Stapel sagen, wo erzeugeKarteInSzene die Karte unterordnen soll
+        Karte karteInSzene = stapel.erzeugeKarteInSzene(); //GameObject zum Zeichnen der Karte in der Szene anlegen
+        karteInSzene.setzeBeschreibung(vorschauKarte.aktuelleBeschreibung(),false); //Kartenwert und Darstellung verbinden & sofort aufdecken
+        vorschauKarte.setzeBeschreibung(null, false);
+        KlickAufStapel();
+
+        container.KartenAnordnen();
+    }
+}
---
+++ Teil 06 - Patience/06a - Basis Kartenausgabe/Scripts/StapelContainer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Ablagefläche für mehrere Karten in Form einer Spalte.
+/// </summary>
+public class StapelContainer : MonoBehaviour
+{
+    /// <summary>
+    /// Berechnet das Layout der Karten im Container. 
+    /// </summary>
+    public void KartenAnordnen()
+    {
+        for(int i=0; i<transform.childCount; i++)
+        {
+            RectTransform rt = transform.GetChild(i).GetComponent<RectTransform>();
+
+            rt.anchorMin = new Vector2(0,1);
+            rt.anchorMax = new Vector2(0, 1);
+            rt.pivot = new Vector2(0, 1);
+
+            rt.anchoredPosition = new Vector2(0,i*-20f);
+            rt.sizeDelta = new Vector2(125,180);
+        }
+    }
+}
