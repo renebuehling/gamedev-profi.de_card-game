e026a45ae59307d8bab722ff381e1ca798f98dab
René Bühling <rene.buehling@gmx.de>
2019-05-14 19:36:11 +0200

    Init

---
+++ Teil 07 - Stapeleditor/Scripts/SpielprinzipPatience.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpielprinzipPatience : MonoBehaviour
+{
+    /// <summary>
+    /// Zeichner für die abgehobene Karte, bevor sie auf dem Tisch platziert wird.
+    /// </summary>
+    public Karte vorschauKarte;
+
+    public GameObject dialogGewonnen;
+    public GameObject dialogFehler;
+
+    private void Start()
+    {
+        Kartenstapel stapel = GetComponent<Kartenstapel>(); //Verweis auf Kartenstapelscript
+        stapel.mischeStapel();
+        KlickAufStapel();
+    }
+
+    public void KlickAufStapel()
+    {
+        Debug.Log("Klick auf Stapel");
+        Kartenstapel stapel = GetComponent<Kartenstapel>(); //Verweis auf Kartenstapelscript
+
+        if (vorschauKarte.aktuelleBeschreibung()!=null)
+        {
+            stapel.legeKarteZurueck(vorschauKarte.aktuelleBeschreibung());
+        }
+
+        if (stapel.istLeer())
+        {
+            stapel.gameObject.SetActive(false);
+        }
+        else
+        {
+            Kartenbeschreibung gezogeneKarte = stapel.hebeObersteKarteAb(); //Karte vom Stapel abheben (nur Wert)
+            vorschauKarte.setzeBeschreibung(gezogeneKarte, false);
+
+            if (stapel.istLeer())
+            {
+                stapel.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private bool WertPasst(Kartenbeschreibung alteKarte, Kartenbeschreibung neueKarte)
+    {
+        if (alteKarte == null) return (neueKarte.Wert == 14);
+        else if (alteKarte.Wert - neueKarte.Wert == 1) return true;
+        else return false;
+    }
+
+    private bool FarbePasst(Kartenbeschreibung alteKarte, Kartenbeschreibung neueKarte)
+    {
+        if (alteKarte == null) return true;
+        else if (Mathf.Abs(alteKarte.Farbe-neueKarte.Farbe) > 10) return true;
+        else return false;
+    }
+
+    public void LegeKarteAuf(StapelContainer container)
+    {
+        if(vorschauKarte.aktuelleBeschreibung()==null) //keine Karte aufgedeckt oder übrig
+        {
+            return;
+        }
+
+        Kartenbeschreibung alteKarte = container.ObersteKarte();
+        Kartenbeschreibung neueKarte = vorschauKarte.aktuelleBeschreibung();
+        if (WertPasst(alteKarte, neueKarte) && FarbePasst(alteKarte,neueKarte))
+        {
+            Debug.Log("Lege die Karte auf " + container);
+            Kartenstapel stapel = GetComponent<Kartenstapel>(); //Verweis auf Kartenstapelscript
+            stapel.kartenContainer = container.gameObject; //dem Stapel sagen, wo erzeugeKarteInSzene die Karte unterordnen soll
+            Karte karteInSzene = stapel.erzeugeKarteInSzene(); //GameObject zum Zeichnen der Karte in der Szene anlegen
+            karteInSzene.setzeBeschreibung(vorschauKarte.aktuelleBeschreibung(), false); //Kartenwert und Darstellung verbinden & sofort aufdecken
+            vorschauKarte.setzeBeschreibung(null, false);
+            KlickAufStapel();
+
+            container.KartenAnordnen();
+
+            if (stapel.istLeer() && vorschauKarte.aktuelleBeschreibung() == null) //alle karten abgelegt
+                dialogGewonnen.SetActive(true);
+        }
+        else //passt nicht 
+        {
+            dialogFehler.SetActive(true);
+        }
+    }
+}
---
+++ Teil 07 - Stapeleditor/Scripts/StapelContainer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Ablagefläche für mehrere Karten in Form einer Spalte.
+/// </summary>
+public class StapelContainer : MonoBehaviour
+{
+    /// <summary>
+    /// Berechnet das Layout der Karten im Container. 
+    /// </summary>
+    public void KartenAnordnen()
+    {
+        for(int i=0; i<transform.childCount; i++)
+        {
+            RectTransform rt = transform.GetChild(i).GetComponent<RectTransform>();
+
+            rt.anchorMin = new Vector2(0,1);
+            rt.anchorMax = new Vector2(0, 1);
+            rt.pivot = new Vector2(0, 1);
+
+            rt.anchoredPosition = new Vector2(0,i*-20f);
+            rt.sizeDelta = new Vector2(125,180);
+        }
+    }
+
+    /// <summary>
+    /// Liefert die oberste Karte auf dem Stapel der Karten in diesem Container.
+    /// </summary>
+    /// <returns>Beschreibung der obersten Karte oder null, wenn der Container noch leer ist.</returns>
+    public Kartenbeschreibung ObersteKarte()
+    {
+        if (transform.childCount == 0) return null;
+        return transform.GetChild(transform.childCount - 1).GetComponent<Karte>().aktuelleBeschreibung();
+    }
+
+}
