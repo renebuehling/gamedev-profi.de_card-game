77c324cfe246eb197f63c22fdcb41f7b7ae98473
René Bühling <rene.buehling@gmx.de>
2019-05-22 14:39:08 +0200

    Init

---
+++ Teil 08 - Klondike/08b Kleine Harfe/Scripts/DropZiel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZiel : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop! "+eventData);
+
+        StapelContainer ziel = GetComponent<StapelContainer>();
+        SpielprinzipPatience spielprinzip = FindObjectOfType<SpielprinzipPatience>();
+        //spielprinzip.LegeKarteAuf(ziel);
+
+        Karte karte = eventData.pointerDrag.GetComponent<Karte>();
+        if (karte != null) //es wurde eine (vorschau)karte gezogen
+        {
+            spielprinzip.LegeKarteAuf(ziel);
+            return; //ondrop abbrechen
+        }
+
+        StapelContainer spalte = eventData.pointerDrag.GetComponent<StapelContainer>();
+        if (spalte!=null) //es wurde eine andere spalte/container gezogen
+        {
+            if(spalte.ziehendeKarten.Count>0) //wenn mehrere karten einer anderen spalte hierher gezogen werden
+            {
+                foreach (Karte k in spalte.ziehendeKarten) Debug.Log("- gezogen wurde: "+k);
+
+                Karte untersteKarte = spalte.ziehendeKarten[0];
+                if (spielprinzip.LegeKarteAuf(ziel, untersteKarte.aktuelleBeschreibung())) //unterste Karte der Ziehenden gemäß der Spielregeln ablegbar?
+                {
+                    for(int i=1; i<spalte.ziehendeKarten.Count ;i++)
+                    {
+                        spielprinzip.LegeKarteAuf(ziel, spalte.ziehendeKarten[i].aktuelleBeschreibung());
+                    }
+                    spalte.EntferneZiehendeKarten();
+                }
+
+            }
+            else if (spielprinzip.LegeKarteAuf(ziel, spalte.ObersteKarte())) //wurde abgelegt
+            {
+                spalte.EntferneObersteKarte();
+            }
+        }
+    }
+}
---
+++ Teil 08 - Klondike/08b Kleine Harfe/Scripts/Kartenstapel.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Zeichnet den Kartenstapel und regelt seine Interaktion.
+/// </summary>
+public class Kartenstapel : MonoBehaviour
+{
+    /// <summary>
+    /// Liste der Karten-Datenobjekte.
+    /// </summary>
+    public List<Kartenbeschreibung> karten;
+
+    /// <summary>
+    /// Vorlage des Szenenobjekts zur Darstellung einer Karte.
+    /// </summary>
+    public GameObject kartenPrefab;
+
+    /// <summary>
+    /// Ziel für die Kopien des kartenPrefabs, einfacher Layoutcontainer.
+    /// </summary>
+    public GameObject kartenContainer;
+
+    
+    /// <summary>
+    /// Testet ob alle Karten aus dem Stapel entnommen wurden.
+    /// </summary>
+    /// <returns>True, wenn keine Karten mehr abgehoben werden können und der Stapel ausgeblendet werden soll.</returns>
+    public bool istLeer()
+    {
+        return karten.Count == 0;
+    }
+
+    /// <summary>
+    /// Wählt eine zufällige Karte aus der Liste karten, entfernt sie aus der Liste
+    /// und liefert sie zurück.
+    /// </summary>
+    /// <returns>Datenbeschreibung der zufällig aus dem Stapel gezogenen Karte.</returns>
+    /// <see cref="erzeugeKarteInSzene"/>
+    public Kartenbeschreibung hebeZufaelligeKarteAb()
+    {
+        int zufaelligeStapelPosition = Random.Range(0, karten.Count);
+
+        Kartenbeschreibung gezogeneKarte = karten[zufaelligeStapelPosition];
+        Debug.Log("Gezogene Karte = " + gezogeneKarte.kartenbild + " von Position=" + zufaelligeStapelPosition);
+        karten.Remove(gezogeneKarte);
+
+        return gezogeneKarte;
+    }
+
+    /// <summary>
+    /// Wählt die oberste Karte aus der Liste karten, entfernt sie aus der Liste
+    /// und liefert sie zurück.
+    /// </summary>
+    /// <returns>Datenbeschreibung der aus dem Stapel gezogenen Karte.</returns>
+    /// <see cref="erzeugeKarteInSzene"/>
+    public Kartenbeschreibung hebeObersteKarteAb()
+    {
+        Kartenbeschreibung gezogeneKarte = karten[0];
+        Debug.Log("Gezogene Karte = " + gezogeneKarte.kartenbild + " von oberster Position");
+        karten.Remove(gezogeneKarte);
+
+        return gezogeneKarte;
+    }
+
+    public Kartenbeschreibung hebeKarteAb(int wert, int farbe)
+    {
+        foreach(Kartenbeschreibung karte in karten)
+        {
+            if (karte.Wert==wert && karte.Farbe==farbe)
+            {
+                karten.Remove(karte);
+                return karte;
+            }
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Erzeugt eine Prefab-Instanz des Kartenprototyps, platziert sie im gewünschten 
+    /// Container und liefert die Karte-Komponente, die die Darstellung steuert.
+    /// </summary>
+    /// <returns>Karten-Darsteller in der Szene.</returns>
+    public Karte erzeugeKarteInSzene()
+    {
+        return Instantiate(kartenPrefab, kartenContainer.transform).GetComponent<Karte>();
+    }
+
+    /// <summary>
+    /// Fügt die Karte am Ende des Stapels ein.
+    /// </summary>
+    /// <param name="karte">Zurückzulegende Karte.</param>
+    public void legeKarteZurueck(Kartenbeschreibung karte)
+    {
+        karten.Add(karte);
+    }
+    
+    /// <summary>
+    /// Mischt den Stapel durch.
+    /// </summary>
+    public void mischeStapel()
+    {
+        List<Kartenbeschreibung> kartenGemischt = new List<Kartenbeschreibung>(); //Ergebnis
+        while(karten.Count>0)
+        {
+            kartenGemischt.Add(hebeZufaelligeKarteAb());
+        }
+        karten = kartenGemischt;
+    }
+}
