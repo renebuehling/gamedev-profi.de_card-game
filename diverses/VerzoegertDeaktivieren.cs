df70fa83fff1d01c27e3ea770495dfdb0de5a0b5
René Bühling <rene.buehling@gmx.de>
2020-01-25 16:17:53 +0100

    Code-Beispiel für Lücken bei Paare Finden

---
+++ diverses/VerzoegertDeaktivieren.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Deaktiviert eine Komponente nach einem Frame. 
+/// 
+/// Anwendungsbeispiel: 
+/// In Abschnitt 6 Paare finden dem Kartenlayout-Objekt hinzufügen und GridLayoutGroup als komponente setzen.
+/// Dadurch bleiben Lücken stehen, wenn Karten entfernt wurden, anstatt das Layout aufzurücken.
+/// </summary>
+public class VerzoegertDeaktivieren : MonoBehaviour
+{
+    [Tooltip("Zeiger auf die Komponente, die deaktiviert werden soll. ")]
+    public Behaviour komponente;
+    
+    IEnumerator Start()
+    {
+        yield return new WaitForEndOfFrame();
+        if (komponente != null) komponente.enabled = false;
+    }
+}
